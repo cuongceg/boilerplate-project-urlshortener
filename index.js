c1964d5d77b7a47b73b1e6e134014050fe490aed
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2025-07-23 08:34:39 +0700

    feat: implement URL shortener API endpoints

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dns = require('dns');
+const urlDatabase = {}
+let index = 0;
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -17,6 +21,34 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl',(req,res)=>{
+  const url = req.body.url;
+  try{
+    const parsedUrl = new URL(url);
+    const hostname = parsedUrl.hostname;
+    dns.lookup(hostname, (err) => {
+      if (err) {
+        return res.json({ error: 'invalid url' });
+      }
+      index++;
+      urlDatabase[index] = url;
+      return res.json({ original_url: url, short_url: index });
+    });
+  }catch(e){
+    return res.json({error: 'invalid url'});
+  }
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+  if (originalUrl) {
+    return res.redirect(originalUrl);
+  } else {
+    return res.json({ error: 'No short URL found for the given input' });
+  }
 });
 
 app.listen(port, function() {
